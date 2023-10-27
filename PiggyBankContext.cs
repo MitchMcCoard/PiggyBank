4c80a37dfa91b44abc7938e6230ad2d41a03930f
Mitch McCoard <mitch.mccoard@gmail.com>
2023-10-27 13:45:58 -0600

    Connected to database

---
+++ PiggyBankContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PiggyBank
+{
+    public class PiggyBankContext : DbContext
+    {
+
+        public PiggyBankContext(DbContextOptions<PiggyBankContext> options)
+            : base(options) { }
+
+        //Should be a dbset, not a property
+        public float CurrentSavings { get; set; }
+       
+    }
+
+    
+}
